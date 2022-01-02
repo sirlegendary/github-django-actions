f5feea87ced487b999937f78a6f124f93217f79f
Wale Salami <walesalami@Wales-MBP-2.lan>
2022-01-02 18:45:53 +0000

    moving storage to s3

--- app/settings.py
+++ app/settings.py
@@ -154,6 +154,7 @@ if USE_S3:
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_QUERYSTRING_AUTH = False
     # AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
